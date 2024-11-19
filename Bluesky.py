083c861908c078e3ff88aff385ecbd7834fda173
Okttenda <118682395+Okttenda@users.noreply.github.com>
2024-11-19 21:45:39 +0100

    Commit 19.11.24 at 21:44
    added:
    - .gitignore
    - .env (sample)
    worken on:
    - Bluesky.py (pre final)

--- Bluesky.py
+++ Bluesky.py
@@ -1,19 +1,35 @@
+import string
 from atproto import Client
 import sys
-
-client = Client()
-client.login('okttenda.bsky.social', '3266-s42a-s4ra-ximv')
-print("Login complete...")
+import os
+from dotenv import load_dotenv
 
 def TestPost():
     post = client.send_post("This is a Test Post sended by a Programm")
     print("Post sended... pasting post.uri and post.cid..." + post.uri + "/n" + post.cid)
 
+def login():
+    load_dotenv()
+    handle = os.getenv("HANDLE")
+    password = os.getenv("PASSWORD")
+    client = Client()
+    try:
+        client.login(handle, password)
+        print("Login complete...")
+    except handle == None or password == None:
+        print("[ERROR]Missing Bluesky Handle or/and Password.")
+        sys.exit()
+    
+def action():
+    print("What do you want to do?")
+    print("Test Post | exit")
+    doing = input()
+    if doing == "Test Post" or doing == 1:
+       TestPost()
+    elif doing == "exit" or doing == 2:
+        sys.exit()
+    else:
+        sys.exit()
 
-print("What do you want to do?")
-print("Test Post | exit")
-doing = input()
-if doing == "Test Post" or 1:
-    TestPost()
-elif doing == "exit" or 2:
-    sys.exit()
+login()
+action()
